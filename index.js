82a57649dd5eb3a28c9a5f229182c3adb1cdfd32
Justin Grasse <just.grasse@gmail.com>
2017-03-06 20:13:25 -0800

    set up handlebars and layouts

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const path = require('path')
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
 
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (request, response) => {
-  throw new Error('oops')
+  response.render('home', {
+    name: 'World'
+  })
 })
 
-app.use((err, request, response, next) => {
-  // log the error, for now just console.log
-  console.log(err)
-  response.status(500).send('Something broke!')
-})
+app.listen(3000)
