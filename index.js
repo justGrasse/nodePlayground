2c247082ef9e0e1710bb41d3ecb92ca62141bdb5
Justin Grasse <just.grasse@gmail.com>
2017-03-06 20:33:00 -0800

    add postgres script in models/db

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const path = require('path')
-const express = require('express')
-const exphbs = require('express-handlebars')
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
 
-const app = express()
+const app = express();
+
+const users = [];
 
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
   layoutsDir: path.join(__dirname, 'views/layouts')
-}))
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (request, response) => {
-  response.render('home', {
+app.get('/', (req, resp) => {
+  resp.render('home', {
     name: 'World'
-  })
-})
+  });
+});
 
-app.listen(3000)
+app.post('/users', function (req, resp) {
+  const user = req.body;
+  users.push({
+    name: user.name,
+    age: user.age
+  });
+  resp.send('successfully registered');
+});
+
+app.listen(3000);
